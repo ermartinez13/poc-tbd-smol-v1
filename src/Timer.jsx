22f39084e9fae8557acf819a5e37637dcdaaaf0e
Erick Martinez <ermartinez13@gmail.com>
2023-10-06 14:49:44 -0400

    fix: naming typo in Timer component props

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,44 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // Timer component that takes a microtask and a function updateMicroTask as props
-const Timer = ({ microtask, updateMicroTask }) => {
-
+const Timer = ({ microTask, updateMicroTask }) => {
   const [timeElapsed, setTimeElapsed] = useState(0);
   const [isActive, setIsActive] = useState(false);
 
   // Function to handle start button
   const handleStart = () => {
     setIsActive(true);
-  }
+  };
 
   // Function to handle stop button
   const handleStop = () => {
     setIsActive(false);
-    updateMicroTask(microtask.id, timeElapsed);
-  }
+    updateMicroTask(microTask.id, timeElapsed);
+  };
 
   // Function to handle reset button
   const handleReset = () => {
     setTimeElapsed(0);
-  }
+  };
 
   // Function to handle extend button
   const handleExtend = () => {
-    updateMicroTask(microtask.id, timeElapsed, microtask.timeBudget * 2);
-  }
+    updateMicroTask(microTask.id, timeElapsed, microTask.timeBudget * 2);
+  };
 
   // Function to handle complete button
   const handleComplete = () => {
-    updateMicroTask(microtask.id, timeElapsed, microtask.timeBudget, 'done');
-  }
+    updateMicroTask(microTask.id, timeElapsed, microTask.timeBudget, "done");
+  };
 
   // useEffect to handle timer increment when isActive is true
   useEffect(() => {
     let interval = null;
-    
+
     if (isActive) {
       interval = setInterval(() => {
-        setTimeElapsed(timeElapsed => timeElapsed + 1);
+        setTimeElapsed((timeElapsed) => timeElapsed + 1);
       }, 1000);
     } else if (!isActive && timeElapsed !== 0) {
       clearInterval(interval);
@@ -49,15 +48,30 @@ const Timer = ({ microtask, updateMicroTask }) => {
 
   return (
     <div id="timer">
-      <h3>{microtask.name}</h3>
-      <p>{Math.floor(timeElapsed / 60)}:{timeElapsed % 60}</p>
-      <button onClick={handleStart} disabled={!isActive && microtask.status === 'done'}>Start</button>
-      <button onClick={handleStop} disabled={!isActive}>Stop</button>
-      <button onClick={handleReset} disabled={isActive || timeElapsed === 0}>Reset</button>
-      <button onClick={handleExtend} disabled={!isActive}>Extend</button>
-      <button onClick={handleComplete} disabled={microtask.status === 'done'}>Complete</button>
+      <h3>{microTask.name}</h3>
+      <p>
+        {Math.floor(timeElapsed / 60)}:{timeElapsed % 60}
+      </p>
+      <button
+        onClick={handleStart}
+        disabled={!isActive && microTask.status === "done"}
+      >
+        Start
+      </button>
+      <button onClick={handleStop} disabled={!isActive}>
+        Stop
+      </button>
+      <button onClick={handleReset} disabled={isActive || timeElapsed === 0}>
+        Reset
+      </button>
+      <button onClick={handleExtend} disabled={!isActive}>
+        Extend
+      </button>
+      <button onClick={handleComplete} disabled={microTask.status === "done"}>
+        Complete
+      </button>
     </div>
   );
-}
+};
 
 export default Timer;

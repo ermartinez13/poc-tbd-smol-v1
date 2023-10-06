0f03ec129252f4a12925961a9b3de094bd3fb1cf
Erick Martinez <ermartinez13@gmail.com>
2023-10-06 14:42:37 -0400

    fix: createTask() args in TaskForm component

--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -1,26 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // The TaskForm component is used to create tasks. It takes in a function prop 'createTask' to handle task creation
 const TaskForm = ({ createTask }) => {
   // Define state variables for the task name and micro budget
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [microBudget, setMicroBudget] = useState(0);
 
   // Handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+
     // Call the createTask function prop
-    createTask({ 
-      name, 
-      microBudget: Number(microBudget),
-      status: 'active',
-      timeSpent: 0,
-      timeBudget: 0
-    });
-    
+    createTask(name, Number(microBudget));
+
     // Reset form fields
-    setName('');
+    setName("");
     setMicroBudget(0);
   };
 
@@ -28,18 +22,27 @@ const TaskForm = ({ createTask }) => {
     <form id="taskForm" onSubmit={handleSubmit}>
       <label>
         Task Name:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
       </label>
-      
+
       <label>
         Micro Budget:
-        <input type="number" value={microBudget} onChange={(e) => setMicroBudget(e.target.value)} required />
+        <input
+          type="number"
+          value={microBudget}
+          onChange={(e) => setMicroBudget(e.target.value)}
+          required
+        />
       </label>
-      
+
       <button type="submit">Create Task</button>
     </form>
   );
 };
 
 export default TaskForm;
-
